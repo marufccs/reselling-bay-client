aa19752d3697ff430dcd1e10ac725ca6f8e7f611
unknown <marufmozumder7@gmail.com>
2022-11-25 00:21:32 +0100

    delete-button-added

--- src/Pages/Dashboard/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/Dashboard/AllSellers/AllSellers.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 import Loader from '../../../../Shared/Loader/Loader';
 import AllSeller from './AllSeller';
 
@@ -14,8 +15,58 @@ const AllSellers = () => {
         }
     })
 
+const [sellerList, setSellerList] = useState(sellers);
+
     if(isLoading){
         return <Loader/>
+    }
+
+    // Sweet Alert
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      const handleDelete = (id) => {
+        swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to undo this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+              swalWithBootstrapButtons.fire(
+                'Deleted!',
+                'Your file has been successfully deleted.',
+                'success'
+              )
+              fetch(`http://localhost:5000/users/sellers/${id}`,{
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0){
+                    const remaining = sellerList.filter(seller => seller._id !== id);
+                        setSellerList(remaining);
+                        refetch();
+                }
+            })
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire(
+                'Cancelled',
+                "Your file hasn't been deleted :)",
+                'error'
+              )
+            }
+          })       
     }
 
     const handleVerify = id => {
@@ -37,6 +88,7 @@ const AllSellers = () => {
         <th>Name</th>
         <th>Email</th>
         <th>Verified</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -47,10 +99,11 @@ const AllSellers = () => {
             <td>{seller.email}</td>
           {
             seller && seller.verified?
-             <button className='btn' disabled>Verified</button>
+             <td><button className='btn' disabled>Verified</button></td> 
              :
              <td> <button onClick={() => handleVerify(seller._id)} className='btn btn-accent text-white'>Verify</button> </td>
           }
+           <td> <button onClick={()=> handleDelete(seller._id)}  className='btn btn-error text-white'>Delete</button> </td>
           </tr>)
       }
     </tbody>
